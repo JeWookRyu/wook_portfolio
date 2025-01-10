a1352ab55d2076c203042e3ad02d5d8b2455896e
Jewook <wpdnr002@naver.com>
2025-01-11 02:21:44 +0900

    mobile_web

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { FcEngineering } from "react-icons/fc";
 const Header = () => {
   return (
-    <div>
        <h1 className="title">
            <div className="title-icon">
               <FcEngineering className="d-block" />
@@ -11,8 +10,8 @@ const Header = () => {
              <a href="https://github.com/JeWookRyu">류제욱</a>
            </div>   
        </h1>    
-    </div>
   )
 }
 
 export default Header
+
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,31 +1,37 @@
-import React, {useState, useContext } from 'react'
-import Menus from '../components/Menus'
+import React, { useState, useContext } from 'react';
+import Menus from '../components/Menus';
 import { DataContext } from '../context/DataProvider';
 import { RiMenuFill } from "react-icons/ri";
 
 const Nav = () => {
-    const { menu } = useContext(DataContext);
-    const [navActive, setNavActive] = useState('about');
+  const { menu } = useContext(DataContext);
+  const [navActive, setNavActive] = useState('about');
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // 메뉴 열림/닫힘 상태 추가
 
-    return (
+  // 모바일 메뉴 클릭 시 메뉴 열기/닫기
+  const handleMenuToggle = () => {
+    setIsMenuOpen(prevState => !prevState);
+  };
+
+  return (
     <nav className="nav_menu">
-       <ul className="mynav">
-       {
-         menu.map((item, index)=>(
-            <li key={index}>
-               <Menus 
-                  navtitle={item.name} 
-                  navLink={item.link} 
-                  navActive={navActive} 
-                  setNavActive={setNavActive} /> 
-            </li>
-         ))
-        }
-         
-       </ul>
-       <div className="mobile-menu d-block d-md-none"><RiMenuFill /></div> 
+      <ul className={`mynav ${isMenuOpen ? 'open' : ''}`}> {/* isMenuOpen에 따라 클래스 추가 */}
+        {menu.map((item, index) => (
+          <li key={index}>
+            <Menus 
+              navtitle={item.name} 
+              navLink={item.link} 
+              navActive={navActive} 
+              setNavActive={setNavActive} 
+            />
+          </li>
+        ))}
+      </ul>
+      <div className="mobile-menu d-block d-md-none" onClick={handleMenuToggle}>
+        <RiMenuFill />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
